6b026243afcd35ee2ee310f2501472f4e63c4d30
Miguel <m1guelbarroso3oct11@gmail.com>
2026-05-17 16:16:55 +0000

    tamo indo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,9 +6,24 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  //Niterói, carta1
+ char Estado1[1] = 'A';
+ char Codigo_da_carta1[3] = 'A01';
+ char Nome_da_cidade1[20] = "Niteroi";
+ int População1 = 516000;
+ float Area_por_km1 = 133.757;
+ float PIB1 = 66300000000;
+ int Pontos_turisticos1 = 4;
 
+  //São Gonçalo
+char Estado2[1] = 'A'
+ char Codigo_da_carta1[3] = 'A01';
+ char Nome_da_cidade1[20] = "Niteroi";
+ int População1 = 516000;
+ float Area_por_km1 = 133.757;
+ float PIB1 = 66300000000;
+ int Pontos_turisticos1 = 4;
   // Área para entrada de dados
-
   // Área para exibição dos dados da cidade
 
 return 0;
