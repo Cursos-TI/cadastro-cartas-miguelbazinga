bf619982ab20405651a6e96f20b3b8f5cac258a7
Miguel <m1guelbarroso3oct11@gmail.com>
2026-05-18 04:40:25 +0000

    GG

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,42 +6,76 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  //Niterói, carta1
+  //Carta1
  char Estado1 = 'A';
- char Codigo_da_carta1[3] = "A01";
- char Nome_da_cidade1[20] = "Niteroi";
- int População1 = 516000;
- float Area_por_km1 = 133.757;
- float PIB1 = 66300000000;
- int Pontos_turisticos1 = 5;
+ char Codigo_da_carta1[4] = "A01";
+ char Nome_da_cidade1[20];
+ int População1;
+ float Area_por_km1;
+ float PIB1;
+ int Pontos_turisticos1;
 
-  //São Gonçalo
+  //Carta2
  char Estado2 = 'A';
- char Codigo_da_carta2[3] = "A02";
- char Nome_da_cidade2[20] = "São_Gonçalo";
- int População2 = 960652;
- float Area_por_km2 = 248.159;
- float PIB2 = 20300000000;
- int Pontos_turisticos2 = 7;
+ char Codigo_da_carta2[4] = "A02";
+ char Nome_da_cidade2[20];
+ int População2;
+ float Area_por_km2;
+ float PIB2;
+ int Pontos_turisticos2;
+  //Area dos comando
+ printf("Insira o nome da primeira cidade do estado A: \n");
+ scanf("%s", Nome_da_cidade1);
+
+ printf("Insira a população da primeira cidade do estado A: \n");
+ scanf("%d", &População1);
+
+  printf("Insira a área por km² da primeira cidade do estado A: \n");
+ scanf("%f", &Area_por_km1);
+
+  printf("Insira o PIB total da primeira cidade do estado A: \n");
+  scanf("%f", &PIB1);
+
+   printf("Insira tanto de pontos turísticos da primeira cidade do estado A: \n");
+  scanf("%d", &Pontos_turisticos1);
+
+   printf("Insira o nome da segunda cidade do estado A: \n");
+   scanf("%s", Nome_da_cidade2);
+
+    printf("Insira a população da segunda cidade do estado A: \n");
+  scanf("%d", &População2);
+   
+   printf("Insira a área por km² da segunda cidade do estado A: \n");
+ scanf("%f", &Area_por_km2);
+
+  printf("Insira o PIB total da segunda cidade do estado A: \n");
+  scanf("%f", &PIB2);
+
+  printf("Insira tanto de pontos turísticos da segunda cidade do estado A: \n");
+  scanf("%d", &Pontos_turisticos2);
+
   // Área para exibição dos dados da cidade
-  //Niterói
+  
+   //Niterói
  printf("Carta 1\n");
  printf("Estado: %c\n", Estado1);
  printf("Código da carta: %s\n", Codigo_da_carta1);
  printf("Nome da Cidade: %s\n", Nome_da_cidade1);
- printf("População: %d\n", População1);
+ printf("População: %d de habitantes \n", População1);
  printf("Área por km²: %f\n", Area_por_km1);
  printf("PIB: %f\n", PIB1);
  printf("Pontos Turísticos: %d\n", Pontos_turisticos1);
 
- //São Goonçalo
+ printf("\n");
+
+  //São Goonçalo
  printf("Carta 2\n");
  printf("Estado: %c\n", Estado2);
  printf("Código da carta: %s\n", Codigo_da_carta2);
  printf("Nome da Cidade: %s\n", Nome_da_cidade2);
- printf("População: %d\n", População2);
- printf("Área por km²: %f\n", Area_por_km2);
- printf("PIB: %f\n", PIB2);
+ printf("População: %d de habitantes \n", População2);
+ printf("Área por km²: %2.f\n", Area_por_km2);
+ printf("PIB: %2.f\n", PIB2);
  printf("Pontos Turísticos: %d\n", Pontos_turisticos2);
  
 return 0;
